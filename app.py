1afcd02bb38ec0d7cd3a71dc2af085a375cdd11a
Alexandra <sasha-021248@yandex.ru>
2024-10-22 22:52:27 +0200

    add new task logic

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 import telebot
 import os
+from chore import Chore
 
 API_TOKEN = os.environ.get("API_TOKEN")
 
 bot = telebot.TeleBot(API_TOKEN)
+chores = []
 
 
 # Handle '/start' and '/help'
@@ -13,12 +15,19 @@ def send_welcome(message):
 Hi there, I am EchoBot.
 I am here to echo your kind words back to you. Just say anything nice and I'll say the exact same thing to you!\
 """)
+    
+@bot.message_handler(commands=['new'])
+def send_welcome(message):
+    bot.reply_to(message, "Добавляем новое дело. Формат: \n1. Название \n2.Дата следующей уборки \n3. Периодичность")
 
 
-# Handle all other messages with content_type 'text' (content_types defaults to ['text'])
 @bot.message_handler(func=lambda message: True)
 def echo_message(message):
-    bot.reply_to(message, message.text)
-
+    text_task = message.text
+    task_split = text_task.split("\n")
+    chore = Chore(len(chores), task_split[0],task_split[1], task_split[2])
+    chores.append(chore)
+    bot.reply_to(message, f"Добавлено новое дело: {chore.name}")
+    
 
 bot.infinity_polling()
---
+++ chore.py
@@ -0,0 +1,7 @@
+class Chore:
+    def __init__(self, id, name, date, periodicity) -> None:
+        self.id = id
+        self.name = name
+        self.date = date
+        self.periodicity = periodicity
+
